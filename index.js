c2b5f8553fc81e0a7ffdf17bbbef418796f4b4c0
Arman Hasan <rifatarmanhasan@gmail.com>
2025-12-15 22:06:28 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors= require('cors');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 3000
+
+// middlewire
+app.use(express.json());
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+  res.send('E-TutionBD is Running!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
